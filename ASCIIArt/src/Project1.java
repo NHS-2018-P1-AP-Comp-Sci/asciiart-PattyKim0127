40c24a0d1cd2d59093831b005f5f76935e45f1bb
s-kimha <s-kimha@53S50030880.it.bsd405.org>
2018-09-19 08:16:53 -0700

    Turning in ASCII Art Project 1 for Patrick (Hanul) Kim.

---
+++ ASCIIArt/src/Project1.java
@@ -0,0 +1,73 @@
+/**
+ * Unit 1 Project
+ * 
+ * In this project you have the opportunity to create a piece of artwork using ASCII 
+ * (American Standard Code for Information Interchange). Using the already created file 
+ * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
+ * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
+ * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
+ * 
+ * For those of you who have dabbled in coding before, try to contain your code to 
+ * just what we have discussed in this unit.  
+ * 
+ * @author Mr. Rasmussen 
+ *
+ */
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	letterR(); 
+	letterA();
+	letterS();
+	moose();
+	letterN();
+
+	}
+public static void moose() {
+	System.out.println("/\\/\\___/\\/\\");
+	System.out.println("\\___\\_/___/");
+	System.out.println("   /O O\\___");
+	System.out.println("  (_'_'_)   \\>>");
+	System.out.println("    ||______/");
+	System.out.println("   |||   ||");
+	System.out.println("   /||   /|");
+	System.out.println("\n");
+
+}
+public static void letterR() {
+	System.out.println("RRRRR");
+	System.out.println("R    R");
+	System.out.println("R    R");
+	System.out.println("RRRR");
+	System.out.println("R    R");
+	System.out.println("R      R");
+	System.out.println("\n");
+}
+public static void letterA() {
+	System.out.println("   A   ");
+	System.out.println("  A A ");
+	System.out.println(" A   A ");
+	System.out.println("AAAAAAA");
+	System.out.println("A      A");
+	System.out.println("\n");
+}
+public static void letterS() {
+	System.out.println(" SSSS ");
+	System.out.println("SS    ");
+	System.out.println("  SS  ");
+	System.out.println("    SS");
+	System.out.println("   SSS");
+	System.out.println("SSSS  ");
+	System.out.println("\n");
+}
+public static void letterN() {
+	System.out.println("NN    N");
+	System.out.println("N N   N");
+	System.out.println("N  N  N");
+	System.out.println("N   N N");
+	System.out.println("N    NN");	
+}
+}
+
